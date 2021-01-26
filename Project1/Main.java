3f95805cdf944525aaba3388d61b3d6be8e0ad65
Kenny A <58489290+alken2@users.noreply.github.com>
2021-01-26 10:11:41 -0500

    Create Main.java

---
+++ Project1/Main.java
@@ -0,0 +1,107 @@
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Task 1
+		System.out.println("Creating a dynamic navigation stack.");
+		DynamicNavStack<String> test = new DynamicNavStack<>();
+
+		System.out.println(test);
+
+		System.out.println("Size: " + test.size());
+		System.out.println("Capacity: " + test.capacity());
+
+		System.out.println("Pushing an action onto stack.");
+		test.push("action1");
+
+		System.out.println(test);
+
+		System.out.println("Size: " + test.size());
+		System.out.println("Capacity: " + test.capacity());
+
+
+		System.out.println("Performed undo and got: " + test.undo());
+		System.out.println("Top of undo stack: " + test.undoTop());
+		System.out.println("Top of redo stack: " + test.redoTop());
+
+		System.out.println(test);
+
+		System.out.println("Size: " + test.size());
+		System.out.println("Capacity: " + test.capacity());
+
+		System.out.println("Pushing an action onto stack.");
+		test.push("action2");
+
+		System.out.println(test);
+
+		System.out.println("Size: " + test.size());
+		System.out.println("Capacity: " + test.capacity());
+
+		System.out.println("Performed undo and got: " + test.undo());
+		System.out.println("Top of undo stack: " + test.undoTop());
+		System.out.println("Top of redo stack: " + test.redoTop());
+
+		System.out.println(test);
+
+		System.out.println("Size: " + test.size());
+		System.out.println("Capacity: " + test.capacity());
+
+		System.out.println("Pushing 2 actions onto stack.");
+		test.push("action3");
+		test.push("action4");
+
+		System.out.println("Size: " + test.size());
+		System.out.println("Capacity: " + test.capacity());
+
+		System.out.println(test);
+
+		System.out.println("Performed undo and got: " + test.undo());
+		System.out.println("Top of undo stack: " + test.undoTop());
+		System.out.println("Top of redo stack: " + test.redoTop());
+
+		System.out.println(test);
+
+		System.out.println("Performed redo and got: " + test.redo());
+		System.out.println("Top of undo stack: " + test.undoTop());
+		System.out.println("Top of redo stack: " + test.redoTop());
+
+		System.out.println(test);
+
+		System.out.println("Pushing 2 actions onto stack.");
+		test.push("action5");
+		test.push("action6");
+
+		System.out.println("Size: " + test.size());
+		System.out.println("Capacity: " + test.capacity());
+
+		System.out.println(test);
+
+		System.out.println("Performed undo and got: " + test.undo());
+		System.out.println("Top of undo stack: " + test.undoTop());
+		System.out.println("Top of redo stack: " + test.redoTop());
+
+		System.out.println(test);
+
+		System.out.println("Performed undo and got: " + test.undo());
+		System.out.println("Top of undo stack: " + test.undoTop());
+		System.out.println("Top of redo stack: " + test.redoTop());
+
+		System.out.println(test);
+
+		System.out.println("Performed undo and got: " + test.undo());
+		System.out.println("Top of undo stack: " + test.undoTop());
+		System.out.println("Top of redo stack: " + test.redoTop());
+
+		System.out.println(test);
+
+		System.out.println("Pushing an action onto stack.");
+		test.push("action7");
+
+		System.out.println("Size: " + test.size());
+		System.out.println("Capacity: " + test.capacity());
+
+		System.out.println(test);
+	}
+
+}
